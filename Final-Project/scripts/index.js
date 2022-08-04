baf4891c38fd615f6923c825994776d292afa9a3
Chikitonik <chikitonik@gmail.com>
2022-08-04 22:43:19 +0300

    tools are horizontal like a ladder

--- Final-Project/scripts/index.js
+++ Final-Project/scripts/index.js
@@ -1,28 +1,43 @@
 function ButtonMainCanvasClick() {
   document.getElementById('about').scrollIntoView();
 }
-// #region highlight nav bar links
+// #region scroll listener
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav .nav-link');
-
+const animation = document.querySelectorAll('.slide-from-right, .slide-from-left, .flip-appear');
+// highlight nav bar links
 window.addEventListener('scroll', () => {
-  let current = '';
+  let currentSection = '';
   sections.forEach(section => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
     if (window.pageYOffset >= (sectionTop - sectionHeight / 5)) {
-      current = section.getAttribute('id')
+      currentSection = section.getAttribute('id')
     }
+    
   })
-  // console.log(current)
-  // console.log(window.pageYOffset)
+    // console.log(window.pageYOffset)
+    // console.log(currentSection)
   navLi.forEach(li => {
     li.classList.remove('active');
-    if(li.classList.contains(current)){
+    if(li.classList.contains(currentSection)){
       li.classList.add('active');
     }
   } )
+// add class animated
+  animation.forEach(element => {
+    const elementTop = element.offsetTop;
+    var windowHeight = $(window).height()
+    // console.log("windowHeight:", windowHeight)
+    // console.log("elementTop:", elementTop)
+    // console.log("window.pageYOffset:", window.pageYOffset)
+      if (window.pageYOffset >= (elementTop - windowHeight * .3)) {
+      element.classList.add('animated');
+      element.classList.remove('hidden');
+    }
+  })
+
 })
-// #endregion highlight nav bar links
+
 
 
