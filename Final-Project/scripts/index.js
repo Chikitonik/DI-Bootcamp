87aa0d08f7a450a224c0e78c167253efb37e3b88
Chikitonik <chikitonik@gmail.com>
2022-08-14 01:36:11 +0300

    modal carousel works

--- Final-Project/scripts/index.js
+++ Final-Project/scripts/index.js
@@ -14,24 +14,24 @@ window.addEventListener('scroll', () => {
     if (window.pageYOffset >= (sectionTop - sectionHeight / 5)) {
       currentSection = section.getAttribute('id')
     }
-    
+
   })
-    // console.log(window.pageYOffset)
-    // console.log(currentSection)
+  // console.log(window.pageYOffset)
+  // console.log(currentSection)
   navLi.forEach(li => {
     li.classList.remove('active');
-    if(li.classList.contains(currentSection)){
+    if (li.classList.contains(currentSection)) {
       li.classList.add('active');
     }
-  } )
-// add class animated
+  })
+  // add class animated
   animation.forEach(element => {
     const elementTop = element.offsetTop;
     var windowHeight = $(window).height()
     // console.log("windowHeight:", windowHeight)
     // console.log("elementTop:", elementTop)
     // console.log("window.pageYOffset:", window.pageYOffset)
-      if (window.pageYOffset >= (elementTop - windowHeight * .3)) {
+    if (window.pageYOffset >= (elementTop - windowHeight * .3)) {
       element.classList.add('animated');
       element.classList.remove('hidden');
     }
@@ -56,4 +56,29 @@ function activateTab(evt, tabName) {
   document.getElementById(tabName).classList.add('animated');
   evt.currentTarget.className += " active";
 }
+
+
 // #endregion tabs
+function ButtonCardClick(clicked_id) {
+  var image_caption = "";
+  image_caption = image_caption.concat("img/projects/", clicked_id);
+
+  document.getElementById("carousel-item-img-1").src = ""
+  document.getElementById("carousel-item-img-2").src = ""
+  document.getElementById("carousel-item-img-3").src = ""
+  document.getElementById("modal-footer-title").innerText = ""
+  document.getElementById("modal-footer-text").innerText = ""
+  try {
+    document.getElementById("carousel-item-img-1").src = image_caption.concat("_1.png");
+    document.getElementById("carousel-item-img-2").src = image_caption.concat("_2.png");
+    document.getElementById("carousel-item-img-3").src = image_caption.concat("_3.png");
+    // document.getElementById("modal-footer-title").innerText = document.getElementById(clicked_id.concat("-description-title")).textContent;
+    document.getElementById("modal-footer-title").append(document.getElementById(clicked_id.concat("-description-title")).innerHTML);
+    // document.getElementById("modal-footer-text").innerText = document.getElementById(clicked_id.concat("-description-text")).textContent;
+    document.getElementById("modal-footer-text").innerHTML = document.getElementById(clicked_id.concat("-description-text")).innerHTML;
+  }
+  catch (e) {
+    console.log("check for errors in fields of card id:", clicked_id);
+  }
+
+}
