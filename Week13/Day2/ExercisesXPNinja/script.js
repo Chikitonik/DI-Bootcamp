86f54a8337ba1c52dee39893fb6fb65f276aeda4
Chikitonik <chikitonik@gmail.com>
2022-12-03 10:50:21 +0200

    w13d4

--- Week13/Day2/ExercisesXPNinja/script.js
+++ Week13/Day2/ExercisesXPNinja/script.js
@@ -1,32 +1,32 @@
-const users = [];
+// const users = [];
 
-const { faker } = require("@faker-js/faker");
+// const { faker } = require("@faker-js/faker");
 
-for (let i = 0; i < 10; i++) {
-  users.push({
-    Name: faker.name.fullName(),
-    Address: faker.address.country(),
-    Email: faker.address.street(),
-  });
-}
+// for (let i = 0; i < 10; i++) {
+//   users.push({
+//     Name: faker.name.fullName(),
+//     Address: faker.address.country(),
+//     Email: faker.address.street(),
+//   });
+// }
 
-// console.log("users :>> ", users);
+// // console.log("users :>> ", users);
 
-// Use the regular expression module to extract numbers from a string
-let str = "k5k3q2g5z6x9bn";
-// Excepted output : 532569
-const regex = /\d/g;
-console.log("output :>> ", str.match(regex).join(""));
+// // Use the regular expression module to extract numbers from a string
+// let str = "k5k3q2g5z6x9bn";
+// // Excepted output : 532569
+// const regex = /\d/g;
+// console.log("output :>> ", str.match(regex).join(""));
 
-const prompt = require("prompt");
-prompt.get("insert full name", function (err, result) {
-  const fullName = result["insert full name"];
-  const regexDigit = /\d/g;
-  if (fullName.match(regexDigit)) console.log("error - number in name!");
-  const regexSpace = /\s/g;
-  if (fullName.match(regexSpace).length > 1)
-    console.log("error - more than 1 space!");
-  const regexFirstUpperCase = /^[A-Z][a-z]*(?: [A-Z][a-z]*)*$/g;
-  if (!fullName.match(regexFirstUpperCase))
-    console.log("error - The first letter of each name should be upper cased!");
-});
+// const prompt = require("prompt");
+// prompt.get("insert full name", function (err, result) {
+//   const fullName = result["insert full name"];
+//   const regexDigit = /\d/g;
+//   if (fullName.match(regexDigit)) console.log("error - number in name!");
+//   const regexSpace = /\s/g;
+//   if (fullName.match(regexSpace).length > 1)
+//     console.log("error - more than 1 space!");
+//   const regexFirstUpperCase = /^[A-Z][a-z]*(?: [A-Z][a-z]*)*$/g;
+//   if (!fullName.match(regexFirstUpperCase))
+//     console.log("error - The first letter of each name should be upper cased!");
+// });
