a84b8efac7980b6c29495bff0807194cafb499a3
Chikitonik <chikitonik@gmail.com>
2023-01-05 11:17:12 +0200

    w16d5

--- Week16/Day4/daily_challenge/server/server.js
+++ Week16/Day4/daily_challenge/server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.get("/hello", (req, res) => {
