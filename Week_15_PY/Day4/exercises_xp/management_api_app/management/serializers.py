d35457ae1ba286fb50f4cab2100f17c96f86430f
Chikitonik <chikitonik@gmail.com>
2023-09-13 20:40:14 +0300

    15.4d

--- Week_15_PY/Day4/exercises_xp/management_api_app/management/serializers.py
+++ Week_15_PY/Day4/exercises_xp/management_api_app/management/serializers.py
@@ -1,16 +1,28 @@
-from .models import Employee
 from rest_framework import serializers
-from .models import Department
+from .models import Department, Employee, Task
 
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='department-detail')
+
     class Meta:
         model = Department
-        fields = ('id', 'name', 'description')
+        fields = ('url', 'id', 'name', 'description')
 
 
-class EmployeeSerializer(serializers.ModelSerializer):
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='employee-detail')
+
     class Meta:
         model = Employee
-        fields = ('id', 'name', 'email', 'phone_number',
-                  'department', 'projects')
+        fields = ('url', 'id', 'name', 'email',
+                  'phone_number', 'department', 'projects')
+
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='task-detail')
+
+    class Meta:
+        model = Task
+        fields = ('url', 'id', 'name', 'description',
+                  'due_date', 'completed', 'project')
--- Week_15_PY/Day4/exercises_xp/management_api_app/management/views.py
+++ Week_15_PY/Day4/exercises_xp/management_api_app/management/views.py
@@ -2,7 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Department
-from .serializers import DepartmentSerializer
+from rest_framework import generics
+from .models import Department, Employee, Task
+from .serializers import DepartmentSerializer, EmployeeSerializer, TaskSerializer
+from .permissions import IsDepartmentAdmin
 
 
 class DepartmentAPIView(APIView):
@@ -17,3 +20,39 @@ class DepartmentAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DepartmentListView(generics.ListAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [IsDepartmentAdmin]
+
+
+class DepartmentCreateView(generics.CreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [IsDepartmentAdmin]
+
+
+class EmployeeListView(generics.ListAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsDepartmentAdmin]
+
+
+class EmployeeCreateView(generics.CreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsDepartmentAdmin]
+
+
+class TaskRetrieveView(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsDepartmentAdmin]
+
+
+class TaskUpdateView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsDepartmentAdmin]
